c3f262ad1e6c9b435b68174ed11459fe753363b2
Anton Nazarov <antiokh@yandex.ru>
2026-08-29 15:39:46 +0200

    Expose legislation linters

--- legalizer/linters/__init__.py
+++ legalizer/linters/__init__.py
@@ -1,5 +1,12 @@
 from .cross_references import lint_internal_references
 from .defined_terms import lint_defined_terms
+from .legislation import lint_legislation_hierarchy, lint_legislation_preamble
 from .source_governance import lint_source_governance
 
-__all__ = ["lint_defined_terms", "lint_internal_references", "lint_source_governance"]
+__all__ = [
+    "lint_defined_terms",
+    "lint_internal_references",
+    "lint_legislation_hierarchy",
+    "lint_legislation_preamble",
+    "lint_source_governance",
+]
