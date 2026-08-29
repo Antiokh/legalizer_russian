b26c04782916497219c866100b5bacc381585d77
Anton Nazarov <antiokh@yandex.ru>
2026-08-29 15:41:29 +0200

    Expose precision linter

--- legalizer/linters/__init__.py
+++ legalizer/linters/__init__.py
@@ -1,6 +1,7 @@
 from .cross_references import lint_internal_references
 from .defined_terms import lint_defined_terms
 from .legislation import lint_legislation_hierarchy, lint_legislation_preamble
+from .precision import lint_vague_time_references
 from .source_governance import lint_source_governance
 
 __all__ = [
@@ -9,4 +10,5 @@ __all__ = [
     "lint_legislation_hierarchy",
     "lint_legislation_preamble",
     "lint_source_governance",
+    "lint_vague_time_references",
 ]
