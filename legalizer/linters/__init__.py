d4fa8d9d1a591653a31012783a449de02fe1c491
Anton Nazarov <antiokh@yandex.ru>
2026-08-30 11:23:44 +0200

    Export contract and scope linters

--- legalizer/linters/__init__.py
+++ legalizer/linters/__init__.py
@@ -1,19 +1,27 @@
 from .admin_orders import directive_infinitive_occurrences, lint_order_directive_infinitives
+from .contract_parties import lint_party_aliases, party_alias_occurrences
 from .cross_references import lint_internal_references
 from .defined_terms import lint_defined_terms
 from .legislation import lint_legislation_hierarchy, lint_legislation_preamble
+from .modality import modality_occurrences
 from .precision import lint_vague_time_references
+from .scope import lint_condition_exception_scope, scope_marker_occurrences
 from .source_governance import lint_source_governance
 from .word_order import lint_instrumental_attachment
 
 __all__ = [
     "directive_infinitive_occurrences",
+    "lint_condition_exception_scope",
     "lint_defined_terms",
     "lint_instrumental_attachment",
     "lint_internal_references",
     "lint_legislation_hierarchy",
     "lint_legislation_preamble",
     "lint_order_directive_infinitives",
+    "lint_party_aliases",
     "lint_source_governance",
     "lint_vague_time_references",
+    "modality_occurrences",
+    "party_alias_occurrences",
+    "scope_marker_occurrences",
 ]
